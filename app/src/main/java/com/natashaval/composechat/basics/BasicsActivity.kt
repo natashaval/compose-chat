48b8c3e343384c85c90df91dc0c0bbb871cce77a
natashaval <natasha.valentina1998@gmail.com>
2022-07-03 23:54:08 +0700

    compose column

--- app/src/main/java/com/natashaval/composechat/basics/BasicsActivity.kt
+++ app/src/main/java/com/natashaval/composechat/basics/BasicsActivity.kt
@@ -3,7 +3,11 @@ package com.natashaval.composechat.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -28,16 +32,37 @@ class BasicsActivity : ComponentActivity() {
 }
 
 @Composable
-private fun Greeting(name: String) {
-  Surface(color = MaterialTheme.colors.primary) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+  Surface(color = MaterialTheme.colors.background) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+      for (name in names) {
+        Greeting(name = name)
+      }
+    }
   }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Composable
+private fun Greeting(name: String) {
+  Surface(
+    color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    Column(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(24.dp)
+    ) {
+      Text(text = "Hello, ")
+      Text(text = name)
+    }
+  }
+}
+
+@Preview(showBackground = true, name = "Text preview", widthDp = 320)
 @Composable
 fun DefaultPreview() {
-  BasicsCodelabTheme {
-    Greeting(name = "Android")
+  ComposeChatTheme {
+    MyApp()
   }
 }
