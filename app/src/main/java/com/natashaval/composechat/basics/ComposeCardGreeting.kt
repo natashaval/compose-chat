6bab7d0f3b8088235f9285c7fccffd3fb4a1e1df
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-07-05 22:06:55 +0700

    card greeting with icon button

---
+++ app/src/main/java/com/natashaval/composechat/basics/ComposeCardGreeting.kt
@@ -0,0 +1,91 @@
+package com.natashaval.composechat.basics
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.natashaval.composechat.R
+import com.natashaval.composechat.ui.theme.BasicsCodelabTheme
+
+@Composable
+private fun CardGreetingList(names: List<String> = List(1000) { "$it" }) {
+  LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
+      CardGreeting(name = name)
+    }
+  }
+}
+
+@Composable
+private fun CardGreeting(name: String) {
+  Card(
+    backgroundColor = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    CardContent(name)
+  }
+}
+
+@Composable
+private fun CardContent(name: String) {
+  var expanded by remember { mutableStateOf(false) }
+  Row(
+    modifier = Modifier
+      .padding(12.dp)
+      .animateContentSize(
+        animationSpec = spring(
+          dampingRatio = Spring.DampingRatioMediumBouncy,
+          stiffness = Spring.StiffnessLow
+        )
+      )
+  ) {
+    Column(
+      modifier = Modifier
+        .weight(1f)
+        .padding(12.dp)
+    ) {
+      Text(text = "Hello, ")
+      Text(
+        text = name,
+        style = MaterialTheme.typography.h4.copy(
+          fontWeight = FontWeight.ExtraBold
+        )
+      )
+      if (expanded) {
+        Text(text = ("Composem ipsum color sit lazy, padding theme elit, sed do bouncy. ").repeat(4))
+      }
+    }
+    IconButton(onClick = { expanded = !expanded }) {
+      Icon(
+        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+        contentDescription = if (expanded) {
+          stringResource(R.string.show_less)
+        } else {
+          stringResource(R.string.show_more)
+        }
+      )
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCardGreetings() {
+  BasicsCodelabTheme {
+    CardGreetingList()
+  }
+}
