49da5975a42ddc987d31d28dbcab580e467f8f5e
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-07-05 21:44:38 +0700

    animation

--- app/src/main/java/com/natashaval/composechat/basics/BasicsActivity.kt
+++ app/src/main/java/com/natashaval/composechat/basics/BasicsActivity.kt
@@ -3,9 +3,15 @@ package com.natashaval.composechat.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +25,7 @@ class BasicsActivity : ComponentActivity() {
     setContent {
       BasicsCodelabTheme {
         Surface(color = MaterialTheme.colors.background) {
-          Greeting(name = "Android")
+          MyApp()
         }
       }
     }
@@ -28,7 +34,7 @@ class BasicsActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-  var shouldShowOnboarding by remember { mutableStateOf(true) }
+  var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
   if (shouldShowOnboarding) {
     OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
   } else {
@@ -37,9 +43,9 @@ private fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-  Column(modifier = Modifier.padding(vertical = 4.dp)) {
-    for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+  LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
       Greeting(name = name)
     }
   }
@@ -47,8 +53,14 @@ private fun Greetings(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 private fun Greeting(name: String) {
-  var expanded = remember { mutableStateOf(false) }
-  val extraPadding = if(expanded.value) 48.dp else 0.dp
+  var expanded by remember { mutableStateOf(false) }
+  val extraPadding by animateDpAsState(
+    targetValue = if(expanded) 48.dp else 0.dp,
+    animationSpec = spring(
+      dampingRatio = Spring.DampingRatioMediumBouncy,
+      stiffness = Spring.StiffnessLow
+    )
+  )
 
   Surface(
     color = MaterialTheme.colors.primary,
@@ -58,13 +70,13 @@ private fun Greeting(name: String) {
       Column(
         modifier = Modifier
           .weight(1f)
-          .padding(bottom = extraPadding)
+          .padding(bottom = extraPadding.coerceAtLeast(0.dp))
       ) {
         Text(text = "Hello, ")
         Text(text = name)
       }
-      OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-        Text(if (expanded.value) "Show less" else "Show more")
+      OutlinedButton(onClick = { expanded = !expanded }) {
+        Text(if (expanded) "Show less" else "Show more")
       }
     }
   }
